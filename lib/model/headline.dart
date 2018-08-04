6e0e9c3d01eb94a541a003ede1c65aeccc90fc2a
JakeMathews <jake.mathews.email@gmail.com>
2018-08-04 00:44:56 -0400

    Added dynamic loading

--- lib/model/headline.dart
+++ lib/model/headline.dart
@@ -2,6 +2,8 @@ class Headline {
   final String text;
   final String source;
   final String url;
+  final String name;
+  final String subredditName;
 
-  Headline({this.text, this.source, this.url});
+  Headline({this.text, this.source, this.url, this.name, this.subredditName});
 }
--- lib/pages/list_page.dart
+++ lib/pages/list_page.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:lazy_load_scrollview/lazy_load_scrollview.dart';
 import 'package:onions/model/headline.dart';
 import 'package:onions/widgets/headline_view.dart';
 
@@ -19,9 +20,9 @@ class ListPage extends StatefulWidget {
 }
 
 class ListPageState extends State<ListPage> {
+  final Map<String, String> subredditNameMap = {};
+  final List<Headline> headlines = [];
   final List<String> subreddits;
-
-  List<Headline> headlines = [];
 
   ListPageState(this.subreddits);
 
@@ -29,53 +30,85 @@ class ListPageState extends State<ListPage> {
   void initState() {
     super.initState();
 
-    final List<Future<http.Response>> subredditFutures =
-        subreddits.map((final String subreddit) {
-      return getContent(subreddit);
+    _load();
+  }
+
+  Future<http.Response> getContent(final String subreddit, {final String last}) {
+    final Map<String, String> params = {
+      'limit': '20',
+    };
+
+    if (last != null) {
+      params['after'] = last;
+    }
+
+    final Uri uri = new Uri(scheme: 'https', host: 'reddit.com', path: 'r/$subreddit/hot.json', queryParameters: params);
+    return http.get(uri);
+  }
+
+  void _load() {
+    print('Loading more data');
+
+    final List<Future<http.Response>> subredditFutures = subreddits.map((final String subreddit) {
+      return getContent(subreddit, last: subredditNameMap[subreddit]);
     }).toList();
 
     Future.wait(subredditFutures).then((final List<http.Response> responses) {
       setState(() {
+        final List<Headline> preshuffleHeadlines = [];
         for (final http.Response response in responses) {
-          final Map<String, dynamic> jsonResponse =
-              json.decode(response.body) as Map<String, dynamic>;
+          final Map<String, dynamic> jsonResponse = json.decode(response.body) as Map<String, dynamic>;
           //print(jsonResponse);
           for (var post in jsonResponse['data']['children']) {
-            //print(post['data']);
-            //print('https://www.reddit.com${post['data']['permalink']}');
-            headlines.add(new Headline(
+            final Headline headline = new Headline(
+              subredditName: post['data']['subreddit'].toString().toLowerCase(),
               source: 'r/${post['data']['subreddit']}',
               text: post['data']['title'],
               url: 'https://www.reddit.com${post['data']['permalink']}',
-            ));
+              name: post['data']['name'],
+            );
+
+            // TODO: Don't do this every time...
+            subredditNameMap[headline.subredditName] = headline.name;
+
+            preshuffleHeadlines.add(headline);
           }
         }
 
-        headlines.shuffle(new Random());
+        preshuffleHeadlines.shuffle(new Random());
+        headlines.addAll(preshuffleHeadlines);
       });
     });
   }
 
-  Future<http.Response> getContent(final String subreddit) {
-    return http.get("https://reddit.com/r/$subreddit/hot.json?limit=100");
-  }
-
   @override
   Widget build(BuildContext context) {
-    final List<HeadlineView> headLineViews = [];
-    for (final Headline headline in headlines) {
-      headLineViews.add(new HeadlineView(
-        headline: headline,
-      ));
+    Widget body;
+
+    if (headlines.length > 0) {
+      body = new LazyLoadScrollView(
+        child: ListView.builder(
+          itemCount: headlines.length,
+          itemBuilder: (context, position) {
+            return new HeadlineView(
+              headline: headlines[position],
+            );
+          },
+        ),
+        onEndOfPage: _load,
+        scrollOffset: 300,
+      );
+    } else {
+      body = new Center(
+        child: new CircularProgressIndicator(),
+      );
     }
 
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Onions'),
       ),
-      body: new ListView(
-        children: headLineViews,
-      ),
+      body: body,
     );
   }
 }
